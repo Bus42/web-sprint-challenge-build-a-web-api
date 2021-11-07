d3b6d18539096faf61c413a74d9fade1cab1338f
Greg <gbrewton1980@gmail.com>
2021-11-07 14:38:18 -0700

    completes projects router

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,35 @@
-// add middlewares here related to projects
+const { get } = require("./projects-model");
+
+
+function verifyRequestBody(req, res, next) {
+    const { name, description } = req.body;
+    if (!name || !description) {
+        return res.status(404).json({
+            error: 'name and description are required fields'
+        });
+    } else {
+        next();
+    }
+}
+
+async function verifyId(req, res, next) {
+    const { id } = req.params;
+    if (!id) {
+        return res.status(404).json({
+            error: 'id is required'
+        });
+    } else {
+        await get(id)
+            .then(response => {
+                return response ? next() : res.status(404).send({ "message": "project with provided id could not be found" })
+            })
+            .catch(() => {
+                res.status(500).send({ "message": "error occured while fetching project" })
+            });
+    }
+}
+
+module.exports = {
+    verifyRequestBody,
+    verifyId
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,5 +1,6 @@
 // Write your "projects" router here!
 const express = require('express');
+const { verifyRequestBody, verifyId } = require('./projects-middleware');
 const projectsRouter = express.Router();
 const { get, insert, update, remove } = require('./projects-model');
 
@@ -10,31 +11,38 @@ projectsRouter.get(`/`, (req, res) => {
         }).catch(err => res.status(500).send(err))
 })
 
-projectsRouter.get(`/:id`, (req, res) => {
+projectsRouter.get(`/:id`, verifyId, (req, res) => {
     get(req.params.id)
         .then(project => {
             res.status(200).send(project);
         }).catch(err => res.status(500).send(err))
 })
 
-projectsRouter.post('/', (req, res) => {
+projectsRouter.get(`/:id/actions`, verifyId, (req, res) => {
+    get(req.params.id)
+        .then(project => {
+            res.status(200).send(project);
+        }).catch(err => res.status(500).send(err))
+})
+
+projectsRouter.post('/', verifyRequestBody, (req, res) => {
     insert(req.body)
         .then(project => {
             res.status(201).send(project);
         }).catch(err => res.status(500).send(err))
 })
 
-projectsRouter.put(`/:id`, (req, res) => {
+projectsRouter.put(`/:id`, verifyId, verifyRequestBody, (req, res) => {
     update(req.params.id, req.body)
         .then(project => {
             res.status(200).send(project);
         }).catch(err => res.status(500).send(err))
 })
 
-projectsRouter.delete(`/:id`, (req, res) => {
+projectsRouter.delete(`/:id`, verifyId, (req, res) => {
     remove(req.params.id)
-        .then(project => {
-            res.status(200).send(project);
+        .then(() => {
+            res.status(200).send(`Project ${req.params.id} deleted`);
         }).catch(err => res.status(500).send(err))
 })
 
