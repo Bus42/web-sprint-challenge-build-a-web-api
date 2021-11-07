860c66483a64259533cd98b4ac7b0284b7a02694
Greg <gbrewton1980@gmail.com>
2021-11-07 13:20:39 -0700

    get /api/projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,34 @@
 // Write your "projects" router here!
+const express = require('express');
+const projectsRouter = express.Router();
+const { get, insert, update, remove } = require('./projects-model');
+
+projectsRouter.get(`/:id`, (req, res) => {
+    get(req.params.id)
+        .then(project => {
+            res.status(200).send(project || []);
+        }).catch(err => res.status(500).send(err))
+})
+
+projectsRouter.post('/', (req, res) => {
+    insert(req.body)
+        .then(project => {
+            res.status(201).send(project);
+        }).catch(err => res.status(500).send(err))
+})
+
+projectsRouter.put(`/:id`, (req, res) => {
+    update(req.params.id, req.body)
+        .then(project => {
+            res.status(200).send(project);
+        }).catch(err => res.status(500).send(err))
+})
+
+projectsRouter.delete(`/:id`, (req, res) => {
+    remove(req.params.id)
+        .then(project => {
+            res.status(200).send(project);
+        }).catch(err => res.status(500).send(err))
+})
+
+module.exports = projectsRouter;
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+// eslint-disable-next-line no-unused-vars
+const colors = require('colors');
+const projectsRouter = require('./api/projects/projects-router');
+require('dotenv').config();
 
-Sing along:
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+const middleware = [express.json(), morgan('dev'), helmet()];
+const port = process.env.PORT || 9000;
+const app = express();
 
-Pull your server into this file and start it!
-*/
+app.use(middleware);
+app.use('/projects', projectsRouter)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`.yellow.bold);
+});
