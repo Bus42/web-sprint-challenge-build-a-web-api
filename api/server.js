568dcdbdf4a394c6d7739c45e944488f0983e455
Greg <gbrewton1980@gmail.com>
2021-11-07 19:24:10 -0700

    moves server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+// eslint-disable-next-line no-unused-vars
+const colors = require('colors');
+const projectsRouter = require('./projects/projects-router');
+const actionsRouter = require('./actions/actions-router');
+require('dotenv').config();
+
+
+const middleware = [morgan('dev'), helmet()];
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(middleware);
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-// eslint-disable-next-line no-unused-vars
-const colors = require('colors');
-const projectsRouter = require('./api/projects/projects-router');
-const actionsRouter = require('./api/actions/actions-router');
-require('dotenv').config();
-
-
-const middleware = [morgan('dev'), helmet()];
+const app = require("./api/server")
 const port = process.env.PORT || 9000;
-const app = express();
-
-app.use(middleware);
-app.use('/api/projects', projectsRouter);
-app.use('/api/actions', actionsRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`.yellow.bold);
